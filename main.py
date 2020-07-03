b84202803a3c7d68f4209789fb0d56b563486a61
Maxime48 <37154360+Maxime48@users.noreply.github.com>
2020-07-03 15:15:29 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+# bot.py
+import os
+import time
+
+import discord
+from dotenv import load_dotenv
+from discord.utils import get
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+@client.event
+async def on_ready():
+    print("The bot is ready!")
+    await client.change_presence(game=discord.Game(name="Kicking Inactive people"))
+    
+count= 0
+max_sec = 60 
+while True: 
+
+  async def purge_members(ctx):
+    for member in ctx.message.server.members:
+        if not get(member.roles, name='active'):
+            await bot.kick(member)
+
+client.run(TOKEN)
