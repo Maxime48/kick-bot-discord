d90174e730a346fa967733f4717dd7b36f15c11e
Maxime48 <37154360+Maxime48@users.noreply.github.com>
2020-07-03 15:39:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from discord.utils import get
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('NOPE')
 
 client = discord.Client()
 @client.event
@@ -20,11 +20,13 @@ max_sec = 60
 while True: 
     time.sleep(1)
     count += 1
-    await client.change_presence(game=discord.Game(name="Kicking Inactive people in",count,"seconds"))
-    if count == max_sec:
-        async def purge_members(ctx):
-            for member in ctx.message.server.members:
-                if not get(member.roles, name='active'):
-                    await bot.kick(member)
+    @client.event
+    async def on_ready():
+        await client.change_presence(game=discord.Game(name=count))
+        if count == max_sec:
+            async def purge_members(ctx):
+                for member in ctx.message.server.members:
+                    if not get(member.roles, name='active'):
+                        await bot.kick(member)
             
 client.run(TOKEN)
